df7b71810af9db4b5513f44dd822b7625629754e
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-01-09 14:43:31 +0300

    fix api

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -29,7 +29,7 @@ const News = () => {
         .then(result => {
           // SetNews(result.data.articles);
           SetNews(result.data);
-          console.log(result.data);
+          console.log(result.data ,data);
         });
     } catch (error) {}
   };
